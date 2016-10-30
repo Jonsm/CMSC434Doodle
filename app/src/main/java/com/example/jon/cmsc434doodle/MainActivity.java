48abbeb50942e00fa38aa94aa09bc51aeab794f8
Jon <sanmijon@mac.com>
2016-10-30 16:07:11 -0400

    added a popup window for choosing colors

--- app/src/main/java/com/example/jon/cmsc434doodle/MainActivity.java
+++ app/src/main/java/com/example/jon/cmsc434doodle/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.jon.cmsc434doodle;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ActionMenuView;
@@ -7,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ActionMenuView amvMenu;
@@ -32,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         MenuInflater inflater = getMenuInflater();
-        //.inflate(R.menu.leftalign_menu, menu);
         inflater.inflate(R.menu.leftalign_menu, amvMenu.getMenu());
         inflater.inflate(R.menu.rightalign_menu, menu);
         return true;
@@ -41,14 +44,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.color:
-                System.out.println("Color");
+            case R.id.clear:
+                System.out.println("Clear");
+                return true;
+
+            case R.id.brush_1:
+                System.out.println("Brush3");
+                return true;
+
+            case R.id.brush_2:
+                System.out.println("Brush3");
                 return true;
 
             case R.id.brush_3:
-                // User chose the "Favorite" action, mark the current item
-                // as a favorite...
                 System.out.println("Brush3");
+                return true;
+
+            case R.id.color:
+                System.out.println("Color");
+                colorDialog();
                 return true;
 
             default:
@@ -57,5 +71,30 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+
+    private void colorDialog() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Color");
+        // this is set the view from XML inside AlertDialog
+        View alertLayout = getLayoutInflater().inflate(R.layout.color_popup_layout, null);
+        alert.setView(alertLayout);
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.out.println("cancel");
+            }
+        });
+
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.out.println("ok");
+            }
+        });
+        AlertDialog dialog = alert.create();
+        dialog.show();
     }
 }
