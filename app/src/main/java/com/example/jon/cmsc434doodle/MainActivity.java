039c3514c732e549dcb1d9b9480f5cbafbd1e83b
Jon <sanmijon@mac.com>
2016-10-30 14:38:44 -0400

    finished menu

--- app/src/main/java/com/example/jon/cmsc434doodle/MainActivity.java
+++ app/src/main/java/com/example/jon/cmsc434doodle/MainActivity.java
@@ -2,17 +2,28 @@ package com.example.jon.cmsc434doodle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ActionMenuView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private ActionMenuView amvMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.mainToolbar);
+        amvMenu = (ActionMenuView) myToolbar.findViewById(R.id.amvMenu);
+        amvMenu.setOnMenuItemClickListener(new ActionMenuView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return onOptionsItemSelected(menuItem);
+            }
+        });
+
         setSupportActionBar(myToolbar);
         getSupportActionBar().setTitle("");
     }
@@ -20,16 +31,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        MenuInflater inflater = getMenuInflater();
+        //.inflate(R.menu.leftalign_menu, menu);
+        inflater.inflate(R.menu.leftalign_menu, amvMenu.getMenu());
+        inflater.inflate(R.menu.rightalign_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_favorite:
+            case R.id.color:
+                System.out.println("Color");
+                return true;
+
+            case R.id.brush_3:
                 // User chose the "Favorite" action, mark the current item
                 // as a favorite...
+                System.out.println("Brush3");
                 return true;
 
             default:
